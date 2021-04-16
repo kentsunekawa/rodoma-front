30aee4d9b31c9bab9965b794d4a10e031e9e57d9
Ken Tsunekawa <tsunekawaken@gmail.com>
2021-04-16 20:06:45 +0900

    eslint prettier add done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Switch, useHistory } from 'react-router-dom';
-import { initCheck, isInitCheckedSelector, isVisitedSelector } from './state/modules/user';
-import { setIsDoorShow, requestCategoryTree, requestSnsList } from './state/modules/app';
-import MemberRoute from 'pages/MemberRoute';
 import GuestRoute from 'pages/GuestRoute';
 import Home from 'pages/Posts';
 import Users from 'pages/Users';
@@ -19,13 +16,16 @@ import ResetPass from 'pages/ResetPass';
 import NotFound from 'pages/NotFound';
 import Post from 'pages/Posts/_id';
 import PostEdit from 'pages/Posts/_id/Edit';
-import Header from './components/modules/Header';
-import Menu from './components/modules/Menu';
-import SearchPanel from './components/modules/SearchPanel';
-import FixWindow from './components/modules/FixWindow';
-import Message from './components/modules/Message';
-import Door from './components/modules/Door';
-import Loading from './components/modules/Loading';
+import MemberRoute from './pages/MemberRoute';
+import { setIsDoorShow, requestCategoryTree, requestSnsList } from 'state/modules/app';
+import { initCheck, isInitCheckedSelector, isVisitedSelector } from 'state/modules/user';
+import Header from 'components/modules/Header';
+import Menu from 'components/modules/Menu';
+import SearchPanel from 'components/modules/SearchPanel';
+import FixWindow from 'components/modules/FixWindow';
+import Message from 'components/modules/Message';
+import Door from 'components/modules/Door';
+import Loading from 'components/modules/Loading';
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
